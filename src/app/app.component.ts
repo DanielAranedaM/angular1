82d32925496bc4cb45ffbe4600c0a91500bb7427
DanielAranedaM <d.aranedam@duocuc.cl>
2023-08-03 13:11:40 -0400

    primera app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myfirstapp';
+  name : string;
+  email : string;
+  website : string;
+  hobbies : string[];
+  showHobbies: boolean;
+
+  constructor(){
+    this.name = "Daniel";
+    this.email = "dan.ar@gmail.com";
+    this.website = "http://www.google.com";
+    this.hobbies = ['dibujar ','programar ','basketball'];
+    this.showHobbies = false;
+  }
+
+  toggleHobbies(){
+    this.showHobbies = !this.showHobbies;
+  }
+
+  newHobby(hobby: { value: string; }){
+    this.hobbies.push(hobby.value);
+    hobby.value = '';
+    return false;
+  }
 }
